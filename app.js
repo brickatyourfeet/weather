eeaac5b2208d6cc4cf06fbdf58b64736aed12295
Kyle Braden <kyle_braden@msn.com>
2017-10-12 10:30:24 -0700

    continue error handling

--- app.js
+++ app.js
@@ -21,11 +21,16 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
+  if(body === 'undefined') console.log('check google map api url, no connection')
   if (error) {
-    console.log('cant connect to server')
+    console.log('cannot connect to server')
+  } else if (body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find address')
+  } else if (body.status === 'OK'){
+    //console.log(JSON.stringify(body, undefined, 2))
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`latitude: ${body.results[0].geometry.location.lat}`)
+    console.log(`longitude: ${body.results[0].geometry.location.lng}`)
   }
-  //console.log(JSON.stringify(body, undefined, 2))
-  console.log(`Address: ${body.results[0].formatted_address}`)
-  console.log(`latitude: ${body.results[0].geometry.location.lat}`)
-  console.log(`longitude: ${body.results[0].geometry.location.lng}`)
+
 })
