10cd7187671d66ad43d1bc3c4500c5cfb3ae1799
Kyle Braden <kyle_braden@msn.com>
2017-10-12 13:21:02 -0700

    refactor and continue error handling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const request = require('request')
+
 const yargs = require('yargs')
+
+const geocode = require('./geocode/geocode.js')
 
 const argv = yargs
   .options({
@@ -14,23 +16,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv
 
-encodedAddress = encodeURIComponent(argv.address)
-
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  if(body === 'undefined') console.log('check google map api url, no connection')
-  if (error) {
-    console.log('cannot connect to server')
-  } else if (body.status === 'ZERO_RESULTS'){
-    console.log('Unable to find address')
-  } else if (body.status === 'OK'){
-    //console.log(JSON.stringify(body, undefined, 2))
-    console.log(`Address: ${body.results[0].formatted_address}`)
-    console.log(`latitude: ${body.results[0].geometry.location.lat}`)
-    console.log(`longitude: ${body.results[0].geometry.location.lng}`)
+geocode.geocodeAddress(argv.address, (errorMessage, results)=>{
+  if(errorMessage){
+    console.log(errorMessage)
+  }else { //stringify takes 3 params, the object, filter, and spacing
+    console.log(JSON.stringify(results, undefined, 2))
   }
-
 })
