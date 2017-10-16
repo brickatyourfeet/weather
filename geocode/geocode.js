276e40115e843b7e745711d485d8f841167532d4
brickatyourfeet <brickatyourfeet@gmail.com>
2017-10-15 21:04:48 -0700

    add chalk functionality

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,18 +1,19 @@
 const request = require('request')
 
-const geocodeAddress = (address, callback)=>{
+
+const geocodeAddress = (address, callback) => {
   let encodedAddress = encodeURIComponent(address)
 
   request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   }, (error, response, body) => {
-    if(body === 'undefined') console.log('check google map api url, no connection')
+    if (body === 'undefined') console.log('check google map api url, no connection')
     if (error) {
       callback('cannot connect to server')
-    } else if (body.status === 'ZERO_RESULTS'){
+    } else if (body.status === 'ZERO_RESULTS') {
       callback('Unable to find address')
-    } else if (body.status === 'OK'){
+    } else if (body.status === 'OK') {
       callback(undefined, {
         address: body.results[0].formatted_address,
         latitude: body.results[0].geometry.location.lat,
